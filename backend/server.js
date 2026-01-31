49347132f0209d60c5a2733029ce335354e91ee5
Sangram Devadhe <sangramdevadhe@gmail.com>
2026-01-31 22:26:36 +0530

    Configure server for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ app.get("/", (req, res) => {
 });
 
 // Server Port
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
